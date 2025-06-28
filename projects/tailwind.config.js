b7cf352d8ec2289033e815cc278da921c346a72e
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-06-28 13:57:55 +0530

    feat: init projects frontend

---
+++ projects/tailwind.config.js
@@ -0,0 +1,15 @@
+import frappeUIPreset from 'frappe-ui/src/tailwind/preset'
+
+export default {
+	presets: [frappeUIPreset],
+	content: [
+		'./index.html',
+		'./src/**/*.{vue,js,ts,jsx,tsx}',
+		'./node_modules/frappe-ui/src/**/*.{vue,js,ts,jsx,tsx}',
+		'../node_modules/frappe-ui/src/**/*.{vue,js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
---
+++ projects/vite.config.js
@@ -0,0 +1,40 @@
+import vue from '@vitejs/plugin-vue'
+import frappeui from 'frappe-ui/vite'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	define: {
+		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+	},
+	plugins: [
+		vue(),
+		frappeui({
+			frappeProxy: true,
+			lucideIcons: true,
+			jinjaBootData: true,
+			buildConfig: {
+				outDir: '../erpnext/public/projects',
+				indexHtmlPath: `../erpnext/www/projects/index.html`,
+			},
+		}),
+	],
+	server: {
+		allowedHosts: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js'),
+		},
+	},
+	optimizeDeps: {
+		include: [
+			'frappe-ui > feather-icons',
+			'showdown',
+			'tailwind.config.js',
+			'engine.io-client',
+			'highlight.js/lib/core',
+		],
+	},
+})
